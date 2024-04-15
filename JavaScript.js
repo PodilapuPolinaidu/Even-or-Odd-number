00e6d4b48033266c8bb4435bb8d6c8831cbeb22c
poli <xyz@gmail.com>
2024-04-15 07:50:16 +0530

    Even or Odd number

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+// Check Even or Odd
+       if(num%2==0){
+        document.write("The number is Even");
+       }
+       else{
+        document.write("The number is Odd");
+       }
+    let number = (num % 2 == 0)?"even":"odd";
+    document.write(`${number}`);
